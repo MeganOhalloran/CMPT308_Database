c86b78015e60d6d472ef1b86c189769e8dd8cbc1
MeganOhalloran <megan.ohalloran1@marist.edu>
2016-02-05 18:03:12 -0500

    SQL Queries
    Megan O'Halloran
    Feb 3
    Select statements for CAP3 database

---
+++ lab3 SQL queries.sql
@@ -0,0 +1,43 @@
+﻿--Megan O'Halloran
+--Lab3 SQL Queries
+--February 3, 2016
+
+SELECT ordnum, totalUSD
+FROM orders;
+
+SELECT name, city
+FROM agents
+WHERE name = 'Smith';
+
+SELECT pid, name, priceUSD
+FROM products
+WHERE quantity > 208000;
+
+SELECT name, city
+FROM customers
+WHERE city = 'Dallas';
+
+SELECT name
+FROM agents
+WHERE NOT city = 'New York' 
+  AND NOT city = 'Tokyo';
+
+SELECT *
+FROM products
+WHERE NOT city = 'Dallas'
+   AND NOT city = 'Duluth'
+   AND priceUSD >= 1.00;
+
+SELECT *
+FROM orders
+WHERE mon = 'jan' 
+   OR mon = 'mar';
+
+SELECT *
+FROM orders
+WHERE mon = 'feb'
+  AND totalUSD < 500;
+
+SELECT *
+FROM orders
+WHERE cid = 'c005';
